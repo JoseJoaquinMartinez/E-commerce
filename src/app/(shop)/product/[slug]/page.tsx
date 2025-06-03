e06ec48aa9f4d71fcf5ca9d803691d6c0817079a
José Joaquín <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2025-06-03 14:59:19 +0000

    Implement product page with dynamic data fetching and add not found component

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,44 @@
-export default function () {
+import { titleFont } from "@/config/fonts";
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+
+export default function ({ params }: Props) {
+  const { slug } = params;
+
+  const product = initialData.products.find((product) => product.slug === slug);
+  if (!product) {
+    notFound();
+  }
   return (
-    <div>
-      <h1>Product # Page</h1>
+    <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
+      {/* Slideshow */}
+      <div className="col-span-1 md:col-span-2 ">
+        <h1>holiwwiwiwiw</h1>
+      </div>
+      {/* Product details */}
+      <div className="col-span-1 px-5">
+        <h1 className={`${titleFont.className} antialiased font-bold- text-xl`}>
+          {product.title}
+        </h1>
+        <p className="text-lg mb-5">{product.price}€</p>
+
+        {/* Size selector */}
+
+        {/* Quantity selector */}
+
+        {/* Buy button */}
+        <button className="btn-primary my-5">Añadir al carrito</button>
+
+        {/* Description */}
+        <h3 className="font-bold text-sm">Descripción</h3>
+        <p className="font-light">{product.description}</p>
+      </div>
     </div>
   );
 }
