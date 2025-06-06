ad08745385acd4b35cf43e1e34cd9b66d30d054a
José Joaquín <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2025-06-06 06:22:45 +0000

    Refactor authentication layout components and add navigation links

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -3,5 +3,9 @@ export default function ShopLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <main className="bg-red-500 min-h-screen ">{children}</main>;
+  return (
+    <main className="flex justify-between">
+      <div className="w-full sm:w-[350px] px-10">{children}</div>
+    </main>
+  );
 }
