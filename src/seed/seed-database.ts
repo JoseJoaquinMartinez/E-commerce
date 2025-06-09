f9838fcc80cb68d61a35b96e35cbe4b04520ceaf
José Joaquín <jj.martinez87@hotmail.com>
2025-06-09 10:11:31 +0200

    feat: implement Prisma client singleton and seed database script
    - Added a singleton pattern for Prisma client in src/lib/prisma.ts to ensure a single instance is used across the application.
    - Created a seed-database script in src/seed/seed-database.ts to populate the database with initial data, including categories and products.
    - Configured TypeScript settings for the seed script in src/seed/tsconfig.json to enable strict type checking and module interoperability.

---
+++ src/seed/seed-database.ts
@@ -0,0 +1,62 @@
+import { initialData } from "./seed";
+import prisma from "../lib/prisma";
+
+async function main() {
+  //Delete existing data
+
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+  /* await Promise.all([
+    prisma.productImage.deleteMany(),
+    prisma.product.deleteMany(),
+    prisma.category.deleteMany(),
+  ]); */
+
+  const { categories, products } = initialData;
+  //Seed categories
+  const categoriesData = categories.map((name) => ({ name }));
+
+  await prisma.category.createMany({
+    data: categoriesData,
+  });
+
+  //Seed products
+  const categoriesDB = await prisma.category.findMany();
+  const categoriesMap = categoriesDB.reduce(
+    (map: { [x: string]: any }, category: { name: string; id: any }) => {
+      map[category.name.toLowerCase()] = category.id;
+
+      return map;
+    },
+    {} as Record<string, string>
+  ); //<string=label, string=id>
+
+  products.forEach(async (productc) => {
+    const { images, type, ...rest } = productc;
+    //Category
+
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type],
+      },
+    });
+
+    //Images
+    const imagesData = images.map(async (image) => ({
+      url: image,
+      productId: dbProduct.id,
+    }));
+
+    await prisma.productImage.createMany({
+      data: imagesData,
+    });
+  });
+
+  console.log("Seeding complete!");
+}
+
+(() => {
+  main();
+})();
