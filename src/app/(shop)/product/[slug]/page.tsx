1e0042801cae136b8827c06bfa69f24896878191
José Joaquín <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2025-06-11 15:23:30 +0000

    refactor: improve code readability and formatting in ProductPage component

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -36,58 +36,56 @@ export async function generateMetadata(
       description: product?.description ?? "Descripción no disponible",
       images: [`/products/${product?.images[1]}`],
     },
-  };}
+  };
+}
 
-  export default async function ProductPage({ params }: Props) {
-    const { slug } = params;
+export default async function ProductPage({ params }: Props) {
+  const { slug } = params;
 
-    const product = await getProductBySlug(slug);
-    if (!product) {
-      notFound();
-    }
-    return (
-      <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
-        <div className="col-span-1 md:col-span-2 ">
-          {/* Mobile Slideshow */}
-          <ProductMobileSlideshow
-            title={product.title}
-            images={product.images}
-            className="block md:hidden"
-          />
-          {/*Desktop Slideshow */}
-          <ProductSlideshow
-            title={product.title}
-            images={product.images}
-            className="hidden md:block"
-          />
-        </div>
-
-        {/* Product details */}
-        <div className="col-span-1 px-5">
-          <StockLabel slug={product.slug} />
-          <h1
-            className={`${titleFont.className} antialiased font-bold- text-xl`}
-          >
-            {product.title}
-          </h1>
-          <p className="text-lg mb-5">{product.price}€</p>
-
-          {/* Size selector */}
-          <SizeSelector
-            selectedSize={product.sizes[0]}
-            availableSizes={product.sizes}
-          />
-          {/* Quantity selector */}
-          <QuantitySelector quantity={2} />
-
-          {/* Buy button */}
-          <button className="btn-primary my-5">Añadir al carrito</button>
-
-          {/* Description */}
-          <h3 className="font-bold text-sm">Descripción</h3>
-          <p className="font-light">{product.description}</p>
-        </div>
-      </div>
-    );
+  const product = await getProductBySlug(slug);
+  if (!product) {
+    notFound();
   }
+  return (
+    <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
+      <div className="col-span-1 md:col-span-2 ">
+        {/* Mobile Slideshow */}
+        <ProductMobileSlideshow
+          title={product.title}
+          images={product.images}
+          className="block md:hidden"
+        />
+        {/*Desktop Slideshow */}
+        <ProductSlideshow
+          title={product.title}
+          images={product.images}
+          className="hidden md:block"
+        />
+      </div>
+
+      {/* Product details */}
+      <div className="col-span-1 px-5">
+        <StockLabel slug={product.slug} />
+        <h1 className={`${titleFont.className} antialiased font-bold- text-xl`}>
+          {product.title}
+        </h1>
+        <p className="text-lg mb-5">{product.price}€</p>
+
+        {/* Size selector */}
+        <SizeSelector
+          selectedSize={product.sizes[0]}
+          availableSizes={product.sizes}
+        />
+        {/* Quantity selector */}
+        <QuantitySelector quantity={2} />
+
+        {/* Buy button */}
+        <button className="btn-primary my-5">Añadir al carrito</button>
+
+        {/* Description */}
+        <h3 className="font-bold text-sm">Descripción</h3>
+        <p className="font-light">{product.description}</p>
+      </div>
+    </div>
+  );
 }
