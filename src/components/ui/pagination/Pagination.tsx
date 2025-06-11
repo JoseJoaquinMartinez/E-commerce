c9662cf7543082cd15acc85dd162ec5fa3179f94
José Joaquín <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2025-06-11 13:14:45 +0000

    feat: refactor page components for better readability and structure; add gender-based routing and pagination logic

--- src/components/ui/pagination/Pagination.tsx
+++ src/components/ui/pagination/Pagination.tsx
@@ -1,6 +1,8 @@
 "use client";
+import { generatePaginationNumbers } from "@/utils";
+import clsx from "clsx";
 import Link from "next/link";
-import { usePathname, useSearchParams } from "next/navigation";
+import { redirect, usePathname, useSearchParams } from "next/navigation";
 import React from "react";
 import {
   IoChevronBackOutline,
@@ -14,7 +16,16 @@ interface Props {
 export const Pagination = ({ totalPages }: Props) => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const currentPage = Number(searchParams.get("page")) ?? 1;
+
+  const pageString = searchParams.get("page") ?? 1;
+
+  const currentPage = isNaN(+pageString) ? 1 : +pageString;
+
+  if (currentPage < 1 || isNaN(+pageString)) {
+    redirect(`${pathname}`);
+  }
+
+  const allPages = generatePaginationNumbers(currentPage, totalPages);
 
   const createPageUrl = (pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams);
@@ -46,30 +57,23 @@ export const Pagination = ({ totalPages }: Props) => {
               <IoChevronBackOutline className="inline-block" />
             </Link>
           </li>
-          <li className="page-item">
-            <a
-              className="page-link relative block py-1.5 px-3  border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-              href="#"
-            >
-              1
-            </a>
-          </li>
-          <li className="page-item active">
-            <a
-              className="page-link relative block py-1.5 px-3  border-0 bg-blue-600 outline-none transition-all duration-300 rounded text-white hover:text-white hover:bg-blue-600 shadow-md focus:shadow-md"
-              href="#"
-            >
-              2 <span className="visually-hidden"></span>
-            </a>
-          </li>
-          <li className="page-item">
-            <a
-              className="page-link relative block py-1.5 px-3  border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-              href="#"
-            >
-              3
-            </a>
-          </li>
+          {allPages.map((page, index) => (
+            <li className="page-item" key={page + "-" + index}>
+              <a
+                className={clsx(
+                  "page-link relative block py-1.5 px-3  border-0  outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none",
+                  {
+                    "bg-blue-600 shadow-md text-white hover:text-white hover:bg-blue-700":
+                      currentPage === page,
+                  }
+                )}
+                href={createPageUrl(page)}
+              >
+                {page}
+              </a>
+            </li>
+          ))}
+
           <li className="page-item">
             <Link
               className="page-link relative block py-1.5 px-3  border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
