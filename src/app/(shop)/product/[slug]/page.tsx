746a82e2ef8054ef0fbbd00338c92bc6c18a6069
José Joaquín <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2025-06-11 13:41:48 +0000

    feat: implement getProductBySlug action and update ProductPage to fetch product data

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,8 +1,8 @@
 import { ProductSlideshow, QuantitySelector, SizeSelector } from "@/components";
 import { titleFont } from "@/config/fonts";
-import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
 import { ProductMobileSlideshow } from "../../../../components/product/slideshow/ProductMobileSlideshow";
+import { getProductBySlug } from "@/actions";
 
 interface Props {
   params: {
@@ -10,10 +10,10 @@ interface Props {
   };
 }
 
-export default function ProductPage({ params }: Props) {
+export default async function ProductPage({ params }: Props) {
   const { slug } = params;
 
-  const product = initialData.products.find((product) => product.slug === slug);
+  const product = await getProductBySlug(slug);
   if (!product) {
     notFound();
   }
