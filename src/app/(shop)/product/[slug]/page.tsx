210e94a3b9c533296b74336bc0638262cbc21755
José Joaquín <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2025-06-11 15:22:30 +0000

    feat: update metadata structure for product page and layout; enhance SEO with dynamic titles

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -11,59 +11,83 @@ import { notFound } from "next/navigation";
 import { ProductMobileSlideshow } from "../../../../components/product/slideshow/ProductMobileSlideshow";
 import { getProductBySlug } from "@/actions";
 
+import type { Metadata, ResolvingMetadata } from "next";
+
 interface Props {
   params: {
     slug: string;
   };
 }
 
-export default async function ProductPage({ params }: Props) {
-  const { slug } = params;
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const slug = params.slug;
 
+  // fetch post information
   const product = await getProductBySlug(slug);
-  if (!product) {
-    notFound();
+
+  return {
+    title: product?.title ?? "Producto no encontrado",
+    description: product?.description ?? "Descripción no disponible",
+    openGraph: {
+      title: product?.title ?? "Producto no encontrado",
+      description: product?.description ?? "Descripción no disponible",
+      images: [`/products/${product?.images[1]}`],
+    },
+  };}
+
+  export default async function ProductPage({ params }: Props) {
+    const { slug } = params;
+
+    const product = await getProductBySlug(slug);
+    if (!product) {
+      notFound();
+    }
+    return (
+      <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
+        <div className="col-span-1 md:col-span-2 ">
+          {/* Mobile Slideshow */}
+          <ProductMobileSlideshow
+            title={product.title}
+            images={product.images}
+            className="block md:hidden"
+          />
+          {/*Desktop Slideshow */}
+          <ProductSlideshow
+            title={product.title}
+            images={product.images}
+            className="hidden md:block"
+          />
+        </div>
+
+        {/* Product details */}
+        <div className="col-span-1 px-5">
+          <StockLabel slug={product.slug} />
+          <h1
+            className={`${titleFont.className} antialiased font-bold- text-xl`}
+          >
+            {product.title}
+          </h1>
+          <p className="text-lg mb-5">{product.price}€</p>
+
+          {/* Size selector */}
+          <SizeSelector
+            selectedSize={product.sizes[0]}
+            availableSizes={product.sizes}
+          />
+          {/* Quantity selector */}
+          <QuantitySelector quantity={2} />
+
+          {/* Buy button */}
+          <button className="btn-primary my-5">Añadir al carrito</button>
+
+          {/* Description */}
+          <h3 className="font-bold text-sm">Descripción</h3>
+          <p className="font-light">{product.description}</p>
+        </div>
+      </div>
+    );
   }
-  return (
-    <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
-      <div className="col-span-1 md:col-span-2 ">
-        {/* Mobile Slideshow */}
-        <ProductMobileSlideshow
-          title={product.title}
-          images={product.images}
-          className="block md:hidden"
-        />
-        {/*Desktop Slideshow */}
-        <ProductSlideshow
-          title={product.title}
-          images={product.images}
-          className="hidden md:block"
-        />
-      </div>
-
-      {/* Product details */}
-      <div className="col-span-1 px-5">
-        <StockLabel slug={product.slug} />
-        <h1 className={`${titleFont.className} antialiased font-bold- text-xl`}>
-          {product.title}
-        </h1>
-        <p className="text-lg mb-5">{product.price}€</p>
-
-        {/* Size selector */}
-        <SizeSelector
-          selectedSize={product.sizes[0]}
-          availableSizes={product.sizes}
-        />
-        {/* Quantity selector */}
-        <QuantitySelector quantity={2} />
-
-        {/* Buy button */}
-        <button className="btn-primary my-5">Añadir al carrito</button>
-
-        {/* Description */}
-        <h3 className="font-bold text-sm">Descripción</h3>
-        <p className="font-light">{product.description}</p>
-      </div>
-    </div>
-  );
 }
