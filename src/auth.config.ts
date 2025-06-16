301ef8558ebe5ed823d37e9f9b438d3da94caaf0
José Joaquín <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2025-06-16 13:50:51 +0000

    feat: add Zod validation to NextAuth credentials and configure authentication provider

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,9 +1,32 @@
-import type { NextAuthConfig } from "next-auth";
+import NextAuth, { type NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { z } from "zod";
 
 export const authConfig = {
   pages: {
     signIn: "/auth/login",
     newUser: "/auth/new-account",
   },
-  providers: [],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+        // If the credentials are invalid, return null
+        if (!parsedCredentials.success) return null;
+
+        // Look for the email
+
+        //Compare passwords
+
+        // If the credentials are valid, return a user object
+
+        const { email, password } = parsedCredentials.data;
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
+
+export const { signIn, signOut, auth } = NextAuth(authConfig);
