09c3a3e22e61294e0f5dffc4a0ba605d4ae2dee5
José Joaquín <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2025-06-16 14:06:58 +0000

    feat: implement LoginForm component and authentication logic for user login

---
+++ src/actions/auth/login.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import { signIn } from "@/auth.config";
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    //if everything goes ok it will automatically sing in and refresh the browser
+    await signIn("credentials", Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes("CredentialsSignin")) {
+      return "CredentialsSignin";
+    }
+    throw error;
+  }
+}
