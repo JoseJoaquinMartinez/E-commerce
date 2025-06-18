39b0c0e9f146efaaf398b00791745a747a09fcce
José Joaquín <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2025-06-18 07:20:06 +0000

    feat: enhance authentication callbacks to include user data in JWT and session

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -8,6 +8,21 @@ export const authConfig = {
     signIn: "/auth/login",
     newUser: "/auth/new-account",
   },
+
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.data = user;
+      }
+      return token;
+    },
+    session({ session, token, user }) {
+      session.user = token.data as any;
+
+      return session;
+    },
+  },
+
   providers: [
     Credentials({
       async authorize(credentials) {
