799347077511094edfa4da411c54fb762875249c
José Joaquín <jj.martinez87@hotmail.com>
2025-06-28 12:28:25 +0200

    feat: enhance PayPalButton component with loading state handling

--- src/components/paypal/PayPalButton.tsx
+++ src/components/paypal/PayPalButton.tsx
@@ -1,7 +1,17 @@
 "use client";
-import { PayPalButtons } from "@paypal/react-paypal-js";
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
 import React from "react";
 
 export const PayPalButton = () => {
+  const [{ isPending }] = usePayPalScriptReducer();
+
+  if (isPending) {
+    return (
+      <div className="animate-pulse mb-16">
+        <div className="h-11 bg-gray-300"></div>
+        <div className="h-11 bg-gray-300 mt-2"></div>
+      </div>
+    );
+  }
   return <PayPalButtons />;
 };
