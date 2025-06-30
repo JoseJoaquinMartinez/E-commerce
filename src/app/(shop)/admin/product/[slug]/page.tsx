a285c0226d065de7deaf2c50c86f845bb02d9750
José Joaquín <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2025-06-30 06:36:15 +0000

    feat: create AdminProductPage and ProductForm components for product management

---
+++ src/app/(shop)/admin/product/[slug]/page.tsx
@@ -0,0 +1,28 @@
+import { getProductBySlug } from "@/actions";
+import { Title } from "@/components";
+import { redirect } from "next/navigation";
+import { ProductForm } from "./ui/ProductForm";
+
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+
+export default async function AdminProductPage({ params }: Props) {
+  const { slug } = params;
+
+  const product = await getProductBySlug(slug);
+
+  if (!product) {
+    redirect("/admin/products");
+  }
+  const title = slug === "new" ? "Nuevo Product" : "Editar product";
+  //todo: new product
+  return (
+    <>
+      <Title title={title} />
+      <ProductForm product={product} />
+    </>
+  );
+}
