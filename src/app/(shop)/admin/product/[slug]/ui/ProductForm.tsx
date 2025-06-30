34c9516921baf3a532ff90d8ff5d23e0141dc6a0
José Joaquín <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2025-06-30 14:42:18 +0000

    feat: add size selection functionality with visual feedback in ProductForm

--- src/app/(shop)/admin/product/[slug]/ui/ProductForm.tsx
+++ src/app/(shop)/admin/product/[slug]/ui/ProductForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Category, Product, ProductImage } from "@/interfaces";
+import clsx from "clsx";
 import Image from "next/image";
 import { useForm } from "react-hook-form";
 
@@ -30,6 +31,9 @@ export const ProductForm = ({ product, categories }: Props) => {
     handleSubmit,
     register,
     formState: { isValid },
+    getValues,
+    setValue,
+    watch,
   } = useForm<FormInputs>({
     defaultValues: {
       ...product,
@@ -37,6 +41,15 @@ export const ProductForm = ({ product, categories }: Props) => {
       sizes: product.sizes ?? [],
     },
   });
+
+  watch("sizes");
+
+  const onSizeChange = (size: string) => {
+    const sizes = new Set(getValues("sizes"));
+    sizes.has(size) ? sizes.delete(size) : sizes.add(size);
+
+    setValue("sizes", Array.from(sizes));
+  };
 
   const onSubmit = async (data: FormInputs) => {
     console.log(data);
@@ -136,7 +149,13 @@ export const ProductForm = ({ product, categories }: Props) => {
               // bg-blue-500 text-white <--- si está seleccionado
               <div
                 key={size}
-                className="flex  items-center justify-center w-10 h-10 mr-2 border rounded-md"
+                onClick={() => onSizeChange(size)}
+                className={clsx(
+                  "p-2 border cursor-pointer rounded-md mr-2 mb-2 w-14 transition-all text-center",
+                  {
+                    "bg-blue-500 text-white": getValues("sizes").includes(size),
+                  }
+                )}
               >
                 <span>{size}</span>
               </div>
